9beb594f34274fe1b25481044cf896051a50a977
Naveen kumar <55360964+Naveen13s@users.noreply.github.com>
2022-05-19 16:15:24 +0530

    Add files via upload

---
+++ Basic InputOutput.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a; // variable declarition
+    a=12; // initialisation
+    char c;
+    bool d;
+    float h;
+    
+    
+    cout<<"size of int "<<sizeof(a)<<endl;
+    cout<<"size of char "<<sizeof(c)<<endl;
+    cout<<"size of bool "<<sizeof(d)<<endl;
+    cout<<"size of float "<<sizeof(h)<<endl;
+   
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int money1;  //input/output
+    cin>>money1;
+    
+    int money2;
+    cin>>money2;
+    
+    int total = money1+money2;
+    cout<<total;
+    return 0;
+    
+}
+
+
+// Sinple Output/Input Program.
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int A;
+    int B;
+      
+    cin>>A;
+    cin>>B;
+    
+    
+    int total = A+B;
+    int r = A-B;
+    cout<<total;
+    cout<<r;
+    return 0;
+
+   
+    
+}
+
